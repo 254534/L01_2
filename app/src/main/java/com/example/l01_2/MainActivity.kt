9ca6dec12df27d55e662901a30959e2e630bf653
ksproska <kamila.sproska@gmail.com>
2021-10-24 13:48:14 +0200

    z cz3 dodane radiobuttony w activity2, button1_m ustawiony

--- app/src/main/java/com/example/l01_2/MainActivity.kt
+++ app/src/main/java/com/example/l01_2/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.l01_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1: Button = findViewById(R.id.button1_m)
+        button1.setOnClickListener { view ->
+            val myIntent = Intent(this, Activity2::class.java)
+            startActivity(myIntent)
+        }
     }
 }
