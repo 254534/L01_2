21483d1c659e17895c3381386e271ae38b28bb97
ksproska <kamila.sproska@gmail.com>
2021-10-24 15:43:38 +0200

    button "back" in activity4 works, button 3 in main also

--- app/src/main/java/com/example/l01_2/Activity4.kt
+++ app/src/main/java/com/example/l01_2/Activity4.kt
@@ -2,10 +2,15 @@ package com.example.l01_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Activity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_4)
+    }
+
+    fun finishActivity3(view: View?) {
+        finish()
     }
 }
--- app/src/main/java/com/example/l01_2/MainActivity.kt
+++ app/src/main/java/com/example/l01_2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.l01_2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button1: Button = findViewById(R.id.button1_m)
+        val button3: Button = findViewById(R.id.button3_m)
+
         button1.setOnClickListener { view ->
             val myIntent = Intent(this, Activity2::class.java)
             startActivity(myIntent)
         }
+        button3.setOnClickListener(mainListener)
+    }
+
+    val mainListener = View.OnClickListener { view ->
+        when (view.getId()) {
+            R.id.button3_m -> {
+                val myIntent = Intent(this, Activity4::class.java)
+                startActivity(myIntent)
+            }
+        }
+
     }
 }
