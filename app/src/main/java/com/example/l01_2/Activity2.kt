c63e6b4608c065d154df3e16b1b3785af979926c
ksproska <kamila.sproska@gmail.com>
2021-10-24 16:53:58 +0200

    radiobuttons disapear text

--- app/src/main/java/com/example/l01_2/Activity2.kt
+++ app/src/main/java/com/example/l01_2/Activity2.kt
@@ -4,8 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +12,14 @@ class Activity2 : AppCompatActivity() {
         setContentView(R.layout.activity_2)
         val button1: Button = findViewById(R.id.button1_2)
         val button2: Button = findViewById(R.id.button2_2)
+        val radiobutton1: RadioButton = findViewById(R.id.radioButton1_2)
+        val radiobutton2: RadioButton = findViewById(R.id.radioButton2_2)
+        val toggleButton1: ToggleButton = findViewById(R.id.toggleButton1_2)
         button1.setOnClickListener(act2Listener)
         button2.setOnClickListener(act2Listener)
+        radiobutton1.setOnClickListener(act2Listener)
+        radiobutton2.setOnClickListener(act2Listener)
+        toggleButton1.setOnClickListener(act2Listener)
     }
 
     val act2Listener = View.OnClickListener { view ->
@@ -28,6 +33,17 @@ class Activity2 : AppCompatActivity() {
                 toast.show()
             }
             R.id.button2_2 -> onBackPressed()
+            R.id.radioButton1_2 -> {
+                val textView: TextView = findViewById(R.id.textView1_2)
+                textView.visibility = View.VISIBLE
+            }
+            R.id.radioButton2_2 -> {
+                val textView: TextView = findViewById(R.id.textView1_2)
+                textView.visibility = View.INVISIBLE
+            }
+            R.id.toggleButton1_2 -> {
+
+            }
         }
     }
 }
