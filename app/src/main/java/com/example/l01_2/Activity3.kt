a8bea4bb46bfe582a39302759c56a086241179d8
ksproska <kamila.sproska@gmail.com>
2021-11-07 18:41:45 +0100

    bmi calculator works

--- app/src/main/java/com/example/l01_2/Activity3.kt
+++ app/src/main/java/com/example/l01_2/Activity3.kt
@@ -1,16 +1,58 @@
 package com.example.l01_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 class Activity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_3)
+
+        val buttonBMI = findViewById<Button>(R.id.button_bmi)
+        val weight: EditText = findViewById<EditText>(R.id.act3_edittext4)
+        val height: EditText = findViewById<EditText>(R.id.act3_edittext5)
+        buttonBMI.setOnClickListener { _ ->
+            val valW: Double = weight.text.toString().toDouble()
+            val valH: Double = height.text.toString().toDouble()
+            val bundle = Bundle()
+            bundle.putDouble("valW", valW)
+            bundle.putDouble("valH", valH)
+            val intent = Intent(this, MainActivity5::class.java)
+            intent.putExtras(bundle)
+            startForResult.launch(intent)
+        }
     }
 
+    val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        { result: ActivityResult ->
+            if (result.resultCode == RESULT_OK)
+            {
+                val intent = result.data
+                val valW = intent?.getDoubleExtra("valW",0.0)
+                val valH = intent?.getDoubleExtra("valH",0.0)
+                val valBmi = intent?.getDoubleExtra("bmi",0.0)
+                if (valBmi != null) {
+                    val weight: EditText = findViewById<EditText>(R.id.act3_edittext4)
+                    val height: EditText = findViewById<EditText>(R.id.act3_edittext5)
+                    val bmi: TextView = findViewById<EditText>(R.id.act3_text_bmi)
+
+                    weight.text = Editable.Factory.getInstance().newEditable(valW.toString())
+                    height.text = Editable.Factory.getInstance().newEditable(valH.toString())
+                    bmi.text = Editable.Factory.getInstance().newEditable(valBmi.toString())
+                }
+            }
+        }
+
     fun finishActivity3(view: View?) {
-        finish()
+        onBackPressed()
     }
 }
---
+++ app/src/main/java/com/example/l01_2/MainActivity5.kt
@@ -0,0 +1,55 @@
+package com.example.l01_2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main5)
+
+        val iii: Intent = getIntent()
+        val bundle = iii.extras
+        var valW:Double? = bundle?.getDouble("valW",0.0)
+        var valH:Double? = bundle?.getDouble("valH",0.0)
+
+        val editTextW = findViewById<EditText>(R.id.act5_edittextW)
+        val editTextH = findViewById<EditText>(R.id.act5_edittextH)
+        val editTextBMI = findViewById<TextView>(R.id.act5_textView_bmi)
+
+        editTextW.text = Editable.Factory.getInstance().newEditable(valW.toString())
+        editTextH.text = Editable.Factory.getInstance().newEditable(valH.toString())
+
+        val buttonCalculate = findViewById<Button>(R.id.act5_button_calculate)
+        buttonCalculate.setOnClickListener{ _ ->
+            valW = editTextW.text.toString().toDouble()
+            valH = editTextH.text.toString().toDouble()
+
+            val bmi: Double? = valH?.let { valW?.div(it*it) }
+            fun Double.round(decimals: Int = 2): Double = "%.${decimals}f".format(this).toDouble()
+            var bmid: Double = 0.0
+            if (bmi != null) {
+                bmid = bmi.round(2)
+            }
+            editTextBMI.text = bmid.toString()
+        }
+
+        val buttonAdd = findViewById<Button>(R.id.act5_button_back)
+        buttonAdd.setOnClickListener { _ ->
+            valW = editTextW.text.toString().toDouble()
+            valH = editTextH.text.toString().toDouble()
+            val bmi: Double = editTextBMI.text.toString().toDouble()
+
+            iii.putExtra("valW",valW)
+            iii.putExtra("valH",valH)
+            iii.putExtra("bmi",bmi)
+            setResult(RESULT_OK, iii)
+            onBackPressed()
+        }
+    }
+}
