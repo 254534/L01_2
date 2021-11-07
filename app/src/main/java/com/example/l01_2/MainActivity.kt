6f3d277c9a6c394a8eba44c7421f50f1dc44ae4d
ksproska <kamila.sproska@gmail.com>
2021-11-07 19:02:42 +0100

    bottom navigation added

--- app/src/main/java/com/example/l01_2/MainActivity.kt
+++ app/src/main/java/com/example/l01_2/MainActivity.kt
@@ -5,37 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button1: Button = findViewById(R.id.button1_m)
-        val button3: Button = findViewById(R.id.button3_m)
-
-        // activity set inside method onClickListener, must be in onCreate
-        button1.setOnClickListener { view ->
-            val myIntent = Intent(this, Activity2::class.java)
-            startActivity(myIntent)
-        }
-        button3.setOnClickListener(mainListener)
-    }
-
-    // this method requires adding android:onClick to xml file
-    fun runActivity3(view: View) {
-        val myIntent = Intent(this, Activity3::class.java)
-        startActivity(myIntent)
-    }
-
-    // this is a handler for multiple events, requires setting onClickListener to object beforehand
-    val mainListener = View.OnClickListener { view ->
-        when (view.getId()) {
-            R.id.button3_m -> {
-                val myIntent = Intent(this, Activity4::class.java)
-                startActivity(myIntent)
+        val bnNavView = findViewById<View>(R.id.bottom_nav) as BottomNavigationView
+        bnNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.button1_m -> startActivity(
+                    Intent(this, Activity2::class.java)
+                )
+                R.id.button2_m -> startActivity(
+                    Intent(this, Activity3::class.java)
+                )
+                R.id.button3_m -> startActivity(
+                    Intent(this, Activity4::class.java)
+                )
             }
+            true
         }
-
     }
 }
